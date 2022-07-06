33c7fd8574d7c26ff08b3fb8db6161c4db2460e1
yhjin <1242615901@qq.com>
2022-07-06 11:25:37 +0800

    1111

--- WorldToSql/Form1.Designer.cs
+++ WorldToSql/Form1.Designer.cs
@@ -32,24 +32,17 @@
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
-            this.字段名 = new System.Windows.Forms.Label();
+            this.值 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.tBox_DBName = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.tBox_vName = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.tBox_desc = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.tBox_tbName = new System.Windows.Forms.TextBox();
             this.richTextBox4 = new System.Windows.Forms.RichTextBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(2, 158);
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.richTextBox1.Location = new System.Drawing.Point(6, 22);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(137, 499);
             this.richTextBox1.TabIndex = 1;
@@ -57,9 +50,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(493, 8);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(-29, 527);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(601, 42);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -67,7 +61,8 @@
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(145, 158);
+            this.richTextBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.richTextBox2.Location = new System.Drawing.Point(149, 22);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.Size = new System.Drawing.Size(137, 499);
             this.richTextBox2.TabIndex = 3;
@@ -76,148 +71,72 @@
             // richTextBox3
             // 
             this.richTextBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.richTextBox3.Location = new System.Drawing.Point(431, 158);
+            this.richTextBox3.Location = new System.Drawing.Point(435, 22);
             this.richTextBox3.Name = "richTextBox3";
             this.richTextBox3.Size = new System.Drawing.Size(137, 499);
             this.richTextBox3.TabIndex = 4;
             this.richTextBox3.Text = "";
             // 
-            // 字段名
+            // 值
             // 
-            this.字段名.AutoSize = true;
-            this.字段名.Location = new System.Drawing.Point(36, 143);
-            this.字段名.Name = "字段名";
-            this.字段名.Size = new System.Drawing.Size(71, 12);
-            this.字段名.TabIndex = 5;
-            this.字段名.Text = "Excel字段列";
-            this.字段名.Click += new System.EventHandler(this.label1_Click);
+            this.值.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.值.AutoSize = true;
+            this.值.Location = new System.Drawing.Point(56, 7);
+            this.值.Name = "值";
+            this.值.Size = new System.Drawing.Size(29, 12);
+            this.值.TabIndex = 5;
+            this.值.Text = "字段";
+            this.值.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(184, 143);
+            this.label2.Location = new System.Drawing.Point(211, 7);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.Size = new System.Drawing.Size(29, 12);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Excel注释列";
+            this.label2.Text = "注释";
             // 
             // label3
             // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(472, 143);
+            this.label3.Location = new System.Drawing.Point(484, 7);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.Size = new System.Drawing.Size(53, 12);
             this.label3.TabIndex = 7;
-            this.label3.Text = "生成视图sql";
-            // 
-            // tBox_DBName
-            // 
-            this.tBox_DBName.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.tBox_DBName.Location = new System.Drawing.Point(89, 8);
-            this.tBox_DBName.Name = "tBox_DBName";
-            this.tBox_DBName.Size = new System.Drawing.Size(398, 21);
-            this.tBox_DBName.TabIndex = 8;
-            this.tBox_DBName.Text = "Inroad-YTWH";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(0, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 12);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "数据库名称：";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 79);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 12);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "视图名称：";
-            // 
-            // tBox_vName
-            // 
-            this.tBox_vName.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.tBox_vName.Location = new System.Drawing.Point(89, 74);
-            this.tBox_vName.Name = "tBox_vName";
-            this.tBox_vName.Size = new System.Drawing.Size(475, 21);
-            this.tBox_vName.TabIndex = 11;
-            this.tBox_vName.Text = "V_SafeOperationLicenseRecord";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 107);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 12);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "视图说明：";
-            // 
-            // tBox_desc
-            // 
-            this.tBox_desc.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.tBox_desc.Location = new System.Drawing.Point(89, 104);
-            this.tBox_desc.Name = "tBox_desc";
-            this.tBox_desc.Size = new System.Drawing.Size(475, 21);
-            this.tBox_desc.TabIndex = 13;
-            this.tBox_desc.Text = "CreatTime: 2022/6/20  CreatBy：yhJin";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(36, 48);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(41, 12);
-            this.label6.TabIndex = 14;
-            this.label6.Text = "表名：";
-            // 
-            // tBox_tbName
-            // 
-            this.tBox_tbName.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.tBox_tbName.Location = new System.Drawing.Point(86, 45);
-            this.tBox_tbName.Name = "tBox_tbName";
-            this.tBox_tbName.Size = new System.Drawing.Size(478, 21);
-            this.tBox_tbName.TabIndex = 15;
-            this.tBox_tbName.Text = "SafeOperationLicenseRecord";
+            this.label3.Text = "属性代码";
             // 
             // richTextBox4
             // 
             this.richTextBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.richTextBox4.Location = new System.Drawing.Point(288, 158);
+            this.richTextBox4.Location = new System.Drawing.Point(292, 22);
             this.richTextBox4.Name = "richTextBox4";
             this.richTextBox4.Size = new System.Drawing.Size(137, 499);
-            this.richTextBox4.TabIndex = 16;
+            this.richTextBox4.TabIndex = 8;
             this.richTextBox4.Text = "";
             // 
-            // label7
+            // label1
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(324, 143);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(53, 12);
-            this.label7.TabIndex = 17;
-            this.label7.Text = "是否必填";
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "值域范围";
             // 
             // FORM1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 669);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.richTextBox4);
-            this.Controls.Add(this.tBox_tbName);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.tBox_desc);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.tBox_vName);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(571, 581);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.tBox_DBName);
+            this.Controls.Add(this.richTextBox4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.字段名);
+            this.Controls.Add(this.值);
             this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.button1);
@@ -236,19 +155,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.RichTextBox richTextBox3;
-        private System.Windows.Forms.Label 字段名;
+        private System.Windows.Forms.Label 值;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox tBox_DBName;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox tBox_vName;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox tBox_desc;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox tBox_tbName;
         private System.Windows.Forms.RichTextBox richTextBox4;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WorldToSql/Form1.cs
+++ WorldToSql/Form1.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,30 +28,15 @@ namespace WorldToSql
                     return;
 
                 var a1 = a.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
-                string[] c = new string[a1.Count() + 2];
-                c[0] = $@"
-                        USE [{tBox_DBName.Text.Trim()}];
-                        GO
-                        /******OBJECT  [dbo].[{tBox_vName.Text.Trim()}]  {tBox_desc.Text.Trim()}******/
-                        SET ANSI_NULLS ON;
-                        GO
-                        SET QUOTED_IDENTIFIER ON;
-                        GO
-                        CREATE VIEW [dbo].[{tBox_vName.Text.Trim()}]
-                        AS
-                        select ";
-                c[c.Length - 1] = $@"
-                                  from {tBox_tbName.Text.Trim()}
-                                  GO";
+                string[] c = new string[a1.Count()];
                 for (int i = 0; i < a1.Count(); i++)
                 {
-                    c[i + 1] = $"{a1[i]},--{b[i]} (*{d[i]}*)";
+                    c[i] = $"///<summary>\r\n///{b[i]}--{d[i]}\r\n/// </summary>\r\n public string {a1[i]} "+"{get; set; }";
                 }
-                c[c.Length - 2] = c[c.Length - 2].Replace(',', ' ').Replace('，', ' ');
-                richTextBox3.Lines = c;
-                //richTextBox1.Lines = null;
-                //richTextBox2.Lines = null;
-
+                richTextBox3.Lines=c;
+                richTextBox1.Lines = null;
+                richTextBox2.Lines = null;
+               
             }
             catch (Exception ex)
             {
@@ -61,61 +45,16 @@ namespace WorldToSql
 
         }
 
-        private async void label1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-
-            try
-            {
-                A();
-                B();
-            }
-            catch (Exception rx)
-            {
-
-                MessageBox.Show(rx.ToString());
-            }
             
-        }
-
-        private async Task<string> A()
-        {
-            try
-            {
-                await Task.Delay(2000);
-                //string path = @"C:\Users\22143\Documents\企业微信下载\WXWork\1688856920322966\Cache\File\2022-06\20220628.log"; // 如果测试要改成自己地址
-                //var contents3 = File.ReadAllText(path, Encoding.GetEncoding("GB2312111"));
-                MessageBox.Show("A");
-                return "a";
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("EX");
-
-                return ex.ToString();
-            }
-          
-
-        }
-        private async Task<string> B()
-        {
-            await Task.Delay(2000);
-            MessageBox.Show("B");
-
-            return "b";
-        }
-
-        private async Task D()
-        {
-            await Task.Run(async () =>
-            {
-                var a = await A();
-                MessageBox.Show("结束A", a);
-            });
-            await Task.Run(async () =>
-            {
-                var b = await B();
-                MessageBox.Show("结束B", b);
-            });
+            Dictionary<string, string> a = new Dictionary<string, string>();
+            a.Add("a", "1");
+            a.Add("a", "2");
+            a.Add("a", "3");
+            a.Add("a", "4");
+            a.Add("a", "5");
+            var b = a["a"];
         }
     }
 }
